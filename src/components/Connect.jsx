983cefa6ff04cbda8bb57e070d81daac72d46cd5
sunidhi788 <davinderkathuria39@gmail.com>
2024-01-31 15:24:15 +0530

    hh

---
+++ src/components/Connect.jsx
@@ -0,0 +1,239 @@
+import React, { useState, useEffect } from 'react'
+import { AE_AMOUNT_FORMATS } from '@aeternity/aepp-sdk'
+import useAeternitySDK from './useAeternitySDK'
+import './Connect.css'
+
+//networkId
+const Connect = () => {
+    const { aeSdk, connectToWallet, address, getBalance } = useAeternitySDK();
+    const [isLoading, setIsLoading] = useState(false);
+    const [balance, setBalance] = useState(null);
+    const [spendTo, setSpendTo] = useState('');
+    const [spendAmount, setSpendAmount] = useState('');
+    const [thankYouVisible, setThankYouVisible] = useState(false);
+    const [noAmount, setnoAmount] = useState(false);
+
+
+    let aci = [
+        {
+            "contract": {
+                "functions": [
+                    {
+                        "arguments": [],
+                        "name": "init",
+                        "payable": false,
+                        "returns": "CharityTransfer.state",
+                        "stateful": false
+                    },
+                    {
+                        "arguments": [],
+                        "name": "sendMoney",
+                        "payable": true,
+                        "returns": {
+                            "tuple": []
+                        },
+                        "stateful": true
+                    },
+                    {
+                        "arguments": [],
+                        "name": "getContractDetails",
+                        "payable": false,
+                        "returns": "CharityTransfer.state",
+                        "stateful": false
+                    }
+                ],
+                "kind": "contract_main",
+                "name": "CharityTransfer",
+                "payable": true,
+                "state": {
+                    "record": [
+                        {
+                            "name": "user1",
+                            "type": "address"
+                        },
+                        {
+                            "name": "user2",
+                            "type": "address"
+                        },
+                        {
+                            "name": "amount",
+                            "type": "int"
+                        },
+                        {
+                            "name": "isConfirmed",
+                            "type": "bool"
+                        }
+                    ]
+                },
+                "typedefs": []
+            }
+        }
+    ];
+    let bytecode =
+        "cb_+QE0RgOg9VJwCt/1bunq4G0DoMIjvwztRQHnOmS4btUFfbgIcyPAuQEGuM3+Dv7QsgA3ADcERwBHAAcXDAKCDAKEDAKGDAKIJwwIAP5E1kQfADcANwBVAoIaDoSfAKDV2POv3jVbFg8fneTzQzjhJ42+W4ZvmRZh17lUxKBJaxoOhgAaDoh/AQM//qGAQw0ENwA3ACYIiAcMBPsDpVRyYW5zYWN0aW9uIGFscmVhZHkgY29uZmlybWVkIG9yIHJlamVjdGVkCwAfMAAHDAj7A31BbW91bnQgc2hvdWxkIGJlIGdyZWF0ZXIgdGhhbiAwCwAUCoaGAQM/sy8DEQ7+0LJJZ2V0Q29udHJhY3REZXRhaWxzEUTWRB8RaW5pdBGhgEMNJXNlbmRNb25leYIvAIU3LjQuMAFOSKJN";
+
+
+
+    useEffect(() => {
+        const fetchBalance = async () => {
+            try {
+                setSpendTo('ak_2QaAzpW4w4pnPNDdrgdLHu5v4bGMxVzUD2F16X3aUrayPcBzZm');
+                const balance = await aeSdk.getBalance(address, { format: AE_AMOUNT_FORMATS.AE });
+                setBalance(balance);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+
+        if (address) {
+            fetchBalance();
+        }
+    }, [aeSdk, address, getBalance]);
+
+
+    const updateBalance = async () => {
+        const balance = await aeSdk.getBalance(address, { format: AE_AMOUNT_FORMATS.AE });
+        setBalance(balance);
+    }
+
+
+    const handleConnectClick = async () => {
+        setIsLoading(true);
+        try {
+            await connectToWallet();
+        } catch (error) {
+            if (!(error instanceof Error)) throw error;
+            console.error(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+
+
+    };
+    const get = async () => {
+        const balance = await aeSdk.getBalance(address, { format: AE_AMOUNT_FORMATS.AE });
+
+        setBalance(balance);
+
+    }
+
+    const handleSpendClick = async () => {
+        try {
+            console.log(spendAmount, spendTo);
+            const contract = await aeSdk.initializeContract({ aci, bytecode, address: "ct_VAVZxayCvmd163vQGpGrNRmaZLfVMbRHnxSVAuwovGEDCjBCs" })
+            console.log("contract", contract);
+
+
+            // const sendMoneyResult = await contract.sendMoney("ak_cgWc8Rs7UcrmRu7VMXBCsG3mMaAKMmAnKgPWsWby5S5C5rhc3");
+            // console.log(sendMoneyResult);
+            const options1 = {
+                amount: spendAmount * 1000000000000000000,
+                callData: "",
+                fee: null,
+                gas: null,
+                gasPrice: 1000000000,
+            };
+            const args = [];
+            const options = Object.fromEntries(
+                Object.entries(options1).filter(([, v]) => v != null),
+            );
+
+            contract
+                ?.$call("sendMoney", args, options)
+                .then((result) => {
+                    console.log(result);
+                    // setSpendPromise(result.hash)
+                    get()
+                    setSpendAmount('')
+                    setThankYouVisible(true)
+                    setTimeout(() => setThankYouVisible(false), 5000);
+                    console.log(result);
+
+                });
+
+            // const result = await aeSdk.spend(spendAmount * 1000000000000000000, spendTo);
+
+
+        } catch (error) {
+            console.error(error.message);
+            setThankYouVisible(false)
+        }
+    };
+
+
+    return (
+        <>
+            <div >
+                {address ? (
+                    <React.Fragment>
+
+
+                        <div className='fixed top-60 right-40 p-4 rounded-lg shadow-md w-64 bg-white
+                flex flex-col items-start slide-in-left'>
+                            <label
+                                className='p__cormorant'
+                            >Balance: {balance}</label>
+
+
+                            <div className='p__cormorant'>
+                                <label
+                                    
+                                >Amount:</label>
+                                <input
+                                    className='p__cormorant_amount'
+                                    type='number'
+                                    placeholder='Enter amount'
+                                    value={spendAmount}
+                                    onChange={(e) => { setSpendAmount(e.target.value) }}
+                                />
+                            </div>
+                            {thankYouVisible && (
+                                <div className="p__cormorant">
+                                    Your Order has been confirmed
+                                </div>
+                            )}
+
+                            {noAmount && (
+                                <div className='p__cormorant'>
+                                    Amount must be greator than 0
+                                </div>
+                            )}
+                            <button className='custom__button'
+                                onClick={() => {
+                                    if (spendAmount) {
+                                        handleSpendClick()
+                                    }
+                                    else {
+                                        setnoAmount(true);
+                                        setTimeout(() => setnoAmount(false), 5000);
+                                    }
+                                }}
+                            >
+                                Confirm
+                            </button>
+
+
+                        </div>
+
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <div className='flex flex-row-reverse ...'>
+
+                            <button className='custom__button'
+                                onClick={handleConnectClick} disabled={isLoading}>
+                                {isLoading ? 'Connecting…' : 'Pay for Your Orders'}
+                            </button>
+
+
+
+                        </div>
+                        {isLoading}
+                    </React.Fragment>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Connect;
